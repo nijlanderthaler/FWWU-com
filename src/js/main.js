df541294c351e18a458affd2d4980a1127ebf7eb
Nico Nijland <niconijland@gmail.com>
2018-02-08 14:36:20 +0100

    several changes

--- src/js/main.js
+++ src/js/main.js
@@ -15,23 +15,77 @@ $(window).scroll(function() {
   }
 });
 
+// scrollspy smooth scrolling
+
+$("body").scrollspy({target: ".navbar", offset: 75});
+$("#collapsibleNavbar a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  }
+});
+
 // hide jumbo-text
 $("#hide").click(function(){
-    $(".disappear").hide();
+  $(".disappear").hide();
 });
 
 // make disappear appear again
 $(".close").click(function(){
-    $(".disappear").show();
+  $(".disappear").show();
 });
 
 // toggle disappear onclick to make room for collapsibleNavbar
 $("#toggle-jumbo-text").click(function(){
-    $(".disappear").toggle();
+  $(".disappear").toggle();
+});
+
+// toggle article content
+// needs work, needs to be reworked to one click event controlling multiple buttons seperately
+$("#expand-1").click(function(){
+  $(".article-hide-1").toggle();
+  $(this).text(function(i, text) {
+    return text === "Close" ? "Read more" : "Close";
+  });
+});
+
+$("#expand-2").click(function(){
+  $(".article-hide-2").toggle();
+  $(this).text(function(i, text) {
+    return text === "Close" ? "Read more" : "Close";
+  });
 });
 
 // fitvids.js
 $(document).ready(function(){
-    // Target your .container, .wrapper, .post, etc.
-    $("#teaser").fitVids();
+  // Target your .container, .wrapper, .post, etc.
+  $("#video").fitVids();
+});
+
+// enable tooltips
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// modal with animate.css
+
+$(".modal-content").addClass("animated zoomIn");
+var hideDelay = true;
+$("#myModal").on("hide.bs.modal",function(e){
+if(hideDelay){
+  $(".modal-content").removeClass("animated zoomIn").addClass("animated zoomOut");
+  hideDelay = false;
+   setTimeout(function(){
+        $("#myModal").modal("hide");
+        $(".modal-content").removeClass("animated zoomOut").addClass("animated zoomIn");
+      }, 300);
+      return false;
+  }
+  hideDelay = true;
+  return true;
 });
