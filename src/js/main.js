38b33264ec05ecb9cab6d3055bb3652ac2041ab7
Nico Nijland <niconijland@gmail.com>
2018-03-01 11:14:18 +0100

    postlaunch changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,91 +1,91 @@
 // Custom jQuery
+$(document).ready(function() {
 
-// headroom.js
-$(".fixed-top").headroom({
-  "offset": 525,
-  "tolerance": 5
-});
+  // headroom.js
+  $(".fixed-top").headroom({
+    "offset": 525,
+    "tolerance": 5
+  });
 
-// controls navbar/menu transparency on scroll
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('.header-fixed').addClass('transparent');
-  } else {
-    $('.header-fixed').removeClass('transparent');
-  }
-});
+  // controls navbar/menu transparency on scroll
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.header-fixed').addClass('transparent');
+    } else {
+      $('.header-fixed').removeClass('transparent');
+    }
+  });
 
-// scrollspy smooth scrolling
+  // scrollspy smooth scrolling
 
-$("body").scrollspy({target: ".navbar", offset: 75});
-$("#collapsibleNavbar a").on('click', function(event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-    var hash = this.hash;
-    $("html, body").animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
-      window.location.hash = hash;
+  $("body").scrollspy({target: ".navbar", offset: 75});
+  $("#collapsibleNavbar a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  // hide jumbo-text
+  $("#hide").click(function(){
+    $(".disappear").hide();
+  });
+
+  // make disappear appear again
+  $(".close").click(function(){
+    $(".disappear").show();
+  });
+
+  // toggle disappear onclick to make room for collapsibleNavbar
+  $("#toggle-jumbo-text").click(function(){
+    $(".disappear").toggle();
+  });
+
+  // toggle article content
+  // needs work, needs to be reworked to one click event controlling multiple buttons
+  // seperately --> event.currentTarget/siblings()/
+  $("#expand-1").click(function(){
+    $(".article-hide-1").slideToggle(400);
+    $(this).text(function(i, text) {
+      return text === "Close" ? "Read more" : "Close";
     });
-  }
-});
-
-// hide jumbo-text
-$("#hide").click(function(){
-  $(".disappear").hide();
-});
-
-// make disappear appear again
-$(".close").click(function(){
-  $(".disappear").show();
-});
-
-// toggle disappear onclick to make room for collapsibleNavbar
-$("#toggle-jumbo-text").click(function(){
-  $(".disappear").toggle();
-});
-
-// toggle article content
-// needs work, needs to be reworked to one click event controlling multiple buttons seperately
-$("#expand-1").click(function(){
-  $(".article-hide-1").slideToggle(400);
-  $(this).text(function(i, text) {
-    return text === "Close" ? "Read more" : "Close";
   });
-});
 
-/*$("#expand-?").click(function(){
-  $(".article-hide-2").slideToggle(400);
-  $(this).text(function(i, text) {
-    return text === "Close" ? "Read more" : "Close";
+  /*$("#expand-?").click(function(){
+    $(".article-hide-2").slideToggle(400);
+    $(this).text(function(i, text) {
+      return text === "Close" ? "Read more" : "Close";
+    });
+  });*/
+
+  // fitvids.js
+  $(document).ready(function(){
+    $("#video").fitVids();
   });
-});*/
 
-// fitvids.js
-$(document).ready(function(){
-  // Target your .container, .wrapper, .post, etc.
-  $("#video").fitVids();
-});
+  // enable tooltips
+  $('[data-toggle="tooltip"]').tooltip();
 
-// enable tooltips
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-});
-
-// modal with animate.css
-
-$(".modal-content").addClass("animated zoomIn");
-var hideDelay = true;
-$("#myModal").on("hide.bs.modal",function(e){
-if(hideDelay){
-  $(".modal-content").removeClass("animated zoomIn").addClass("animated zoomOut");
-  hideDelay = false;
-   setTimeout(function(){
-        $("#myModal").modal("hide");
-        $(".modal-content").removeClass("animated zoomOut").addClass("animated zoomIn");
-      }, 200);
-      return false;
-  }
-  hideDelay = true;
-  return true;
+  // modal with animate.css
+  var modalContent = $(".modal-content");
+  modalContent.addClass("animated zoomIn");
+  var hideDelay = true;
+  $("#myModal").on("hide.bs.modal",function(e){
+  if(hideDelay){
+    modalContent.removeClass("animated zoomIn").addClass("animated zoomOut");
+    hideDelay = false;
+     setTimeout(function(){
+          $("#myModal").modal("hide");
+          modalContent.removeClass("animated zoomOut").addClass("animated zoomIn");
+        }, 400);
+        return false;
+    }
+    hideDelay = true;
+    return true;
+  });
 });
